9a742508460a2d57b8ec300b0ff2bca59449e2e1
Bassey Akom <bassi.cim@gmail.com>
2024-12-01 19:50:54 +0000

    binding port

--- app.py
+++ app.py
@@ -134,5 +134,5 @@ def quit():
     return redirect('/')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
