d9fdd18417ea77c576e1cad1e24df8495726ff33
Bassey Akom <bassi.cim@gmail.com>
2024-12-01 17:40:17 +0000

    binding port

--- app.py
+++ app.py
@@ -108,6 +108,6 @@ def quit():
 
 if __name__ == '__main__':
     # Use the PORT environment variable for Heroku
-    port = int(os.environ.get('PORT', 5001))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
